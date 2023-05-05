3b16df275618e7b48531c92df46b0497d7f04aae
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-05-05 15:10:50 +0000

    [finish] Chapter9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      render :new
+      render :new # <= renderのまま ※error messageを表示させるため
     end
   end
 
